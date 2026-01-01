70b4d1211d1726e4dfdea01b2adcdb1890cc7ff4
GTian5418 <96931371+GTian5418@users.noreply.github.com>
2026-01-02 04:29:59 +0800

    Add files via upload

---
+++ lineage_cannon.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from cannon device
+$(call inherit-product, device/xiaomi/cannon/device.mk)
+
+PRODUCT_DEVICE := cannon
+PRODUCT_NAME := lineage_cannon
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := M2007J22C
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="cannong-user 12 SP1A.210812.016 V13.0.3.0.SJEMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/cannong_global/cannong:12/SP1A.210812.016/V13.0.3.0.SJEMIXM:user/release-keys
+PRODUCT_OTACERT := android-certs/releasekey.x509.pem
+PRODUCT_DEFAULT_DEV_CERTIFICATE := android-certs/releasekey
